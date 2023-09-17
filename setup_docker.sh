50f062a192351c4f26c237837eef389e55bddf22
Business1sg00d <112768445+Business1sg00d@users.noreply.github.com>
2023-09-17 15:42:16 -0400

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -23,27 +23,17 @@ check_root
 unzip /var/www/harambo.zip -d /var/www/
 
 # Create backup of /etc/apache2/apache2.conf, then copy secure app conf to same location. 
-if [ ! -f "/etc/apache2/apache2.conf.bak_1" ]
-then
-	echo "Backing up /etc/apache2/apache2.conf. File at /etc/apache2/apache2.conf.bak_1"; echo
-	cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak_1
-	mv /var/www/html/apache2.conf /etc/apache2/apache2.conf
-fi
+echo "Backing up /etc/apache2/apache2.conf. File at /etc/apache2/apache2.conf.bak_1"; echo
+cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak_1
+mv /var/www/html/apache2.conf /etc/apache2/apache2.conf
 
 # Move config.php file to /var/www.
-if [ ! -f "/var/www/config.php" ]
-then
-	echo "config file at /var/www/html/config.php"; echo
-	mv /var/www/html/config.php /var/www/config.php
-fi
+echo "config file at /var/www/html/config.php"; echo
+mv /var/www/html/config.php /var/www/config.php
 
-# Move dir.conf to /etc/apache2/mods-enabled/dir.conf
-if [ ! -f "/etc/apache2/mods-available/dir.conf.bak_1" ]
-then
-	echo "Moving updated dir.conf to /etc/apache2/mods-available/dir.conf. Backing up at /etc/apache2/mods-available/dir.conf.bak_1"; echo
-	cp /etc/apache2/mods-available/dir.conf /etc/apache2/mods-available/dir.conf.bak_1
-	mv /var/www/html/dir.conf /etc/apache2/mods-available/dir.conf
-fi
+echo "Moving updated dir.conf to /etc/apache2/mods-available/dir.conf. Backing up at /etc/apache2/mods-available/dir.conf.bak_1"; echo
+cp /etc/apache2/mods-available/dir.conf /etc/apache2/mods-available/dir.conf.bak_1
+mv /var/www/html/dir.conf /etc/apache2/mods-available/dir.conf
 
 # Change permissions of relevant files and directories to www-data.
 chown -R www-data:www-data /var/www/html/
@@ -51,17 +41,9 @@ chown www-data:www-data /var/www/config.php
 chmod 600 /var/www/config.php
 
 # Create database,table, and least privileged user needed for authentication mechanism.
-sql_process=$(netstat -lptan|grep -P "3306.*?LISTEN")
-
-if [[ -n $sql_process ]]
-then
-	echo "Creating MySQL database \"db1\"."; echo
-	echo "Creating a MySQL service user called \"www-data\""; echo
-	mysql -u'root' -h localhost -e \
-	'CREATE DATABASE db1; USE db1; CREATE TABLE member (userid int(11) NOT null auto_increment, user VARCHAR(256) NOT null, pwd VARCHAR(256) NOT null, PRIMARY key (userid)); CREATE USER "www-data"@"localhost" IDENTIFIED BY "shmeegol_shmorf"; GRANT SELECT ON db1.* TO "www-data"@"localhost"; FLUSH PRIVILEGES; INSERT INTO member VALUES (1, "tester", "$2y$10$.6lMmXtQwyo3qzN1DNdp0OOQpOqv0w9gdAm2BYSqeZIJcMGXp7ajG");'
-	echo "Make sure to add the password you entered for www-data into the config.php file located at /var/www/"; echo
-else
-	echo "Start the mariadb.service in order to run and configure MySQL. If you dont't have it on your system, then \"apt-get install mysql-server\" and re-run \"./setup.sh\"."; echo
-	echo "The mysql service may not be running on the default port 3306. If that's the case, you're on your own here. Gotta change config.php in order to make a connection to the non-default port. Also need to change the setup script mysql command, or add mysql data manually; once done start and enable the apache service, then you should be good to go."; echo
-	exit -1
-fi
+echo "Creating MySQL database \"db1\"."; echo
+echo "Creating a MySQL service user called \"www-data\""; echo
+mysql -u'root' -h localhost -e \
+'CREATE DATABASE db1; USE db1; CREATE TABLE member (userid int(11) NOT null auto_increment, user VARCHAR(256) NOT null, pwd VARCHAR(256) NOT null, PRIMARY key (userid)); CREATE USER "www-data"@"localhost" IDENTIFIED BY "shmeegol_shmorf"; GRANT SELECT ON db1.* TO "www-data"@"localhost"; FLUSH PRIVILEGES; INSERT INTO member VALUES (1, "tester", "$2y$10$.6lMmXtQwyo3qzN1DNdp0OOQpOqv0w9gdAm2BYSqeZIJcMGXp7ajG");'
+echo "Make sure to add the password you entered for www-data into the config.php file located at /var/www/"; echo
+exit 0
